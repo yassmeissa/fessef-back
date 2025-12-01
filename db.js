ebea9d60c505a11ad6831386cae4a6d4a9baf170
Mame Yacine Ndiaye <yassmeissa@MacBook-Air-de-Mame.local>
2025-12-01 18:37:40 +0100

    tyguyhijn

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
 });
